cedc3096979e4e02b00e4f29ebebeac37c210e56
leversol <test_win8@outlook.com>
2017-02-10 20:42:22 +0000

    Initial Commit

---
+++ YoutubeProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YoutubeProject
+//
+//  Created by Abdou on 10/02/2017.
+//  Copyright © 2017 Abdou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YoutubeProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YoutubeProject
+//
+//  Created by Abdou on 10/02/2017.
+//  Copyright © 2017 Abdou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
